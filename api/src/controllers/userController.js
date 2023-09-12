c008338bbaa442be65a100d0dc4d74c14a0056e9
carlosaj98 <carlosaj.arahal@gmail.com>
2023-09-12 16:07:20 +0200

    feat: User controller

---
+++ api/src/controllers/userController.js
@@ -0,0 +1,50 @@
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+
+const User = require("../models/user")
+
+const register = async (req, res) => {
+  const { password: plainTextPassword, ...userDetails } = req.body
+
+  const salt = await bcrypt.genSalt(10)
+  const password = await bcrypt.hash(plainTextPassword, salt)
+
+  const user = await User.create({
+    ...userDetails,
+    password,
+    isAdmin: false,
+  })
+
+  const token = jwt.sign(
+    { username: user.username, isAdmin: user.isAdmin, id: user.id },
+    process.env.jwtPrivateKey
+  )
+  res.setHeader("Access-Control-Expose-Headers", "x-auth-token")
+  res.setHeader("x-auth-token", token)
+  res.send("Usuario registrado y logueado")
+}
+
+const login = async (req, res) => {
+  const { password: plainTextPassword, username } = req.body
+
+  const user = await User.findOne({ username })
+  if (!user) return res.status(400).send("El usuario y contraseña no coincide")
+
+  const isValidUser = await bcrypt.compare(plainTextPassword, user.password)
+  if (!isValidUser)
+    return res.status(400).send("El usuario y contraseña no coincide")
+
+  const token = jwt.sign(
+    { username: user.username, isAdmin: user.isAdmin, id: user.id },
+    process.env.jwtPrivateKey
+  )
+
+  res.setHeader("Access-Control-Expose-Headers", "x-auth-token")
+  res.setHeader("x-auth-token", token)
+  res.send("Usuario logueado")
+}
+
+module.exports = {
+  register,
+  login,
+}
